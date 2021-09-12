a01d05fe8ad01a6ea38483081912b88848992682
Leticia Buss <leticia.buss@contaazul.com>
2021-09-12 16:47:51 -0300

    Inializando um projeto

---
+++ src/main/java/br/com/GerenciadorAcademia/GerenciadorAcademiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.GerenciadorAcademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorAcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/GerenciadorAcademia/GerenciadorAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.GerenciadorAcademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
